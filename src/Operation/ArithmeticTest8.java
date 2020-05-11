788f0707ec6ab4f02bcba2390811fad406d025de
gaomoqi <2215360989@qq.com>
2020-05-11 16:19:03 +0800

    src

---
+++ src/Operation/ArithmeticTest8.java
@@ -0,0 +1,10 @@
+package Operation;
+import Operation.MyExGUI;
+
+public class ArithmeticTest8 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MyExGUI exercise = new MyExGUI();
+    }
+
+}
